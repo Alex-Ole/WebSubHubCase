b6e65de2ea4dc773a014c2a0fcca7406bf31e30f
Alexander Olegård <aolegard@kth.se>
2025-05-24 08:50:14 +0200

    Sub verification now async

--- hub/main.go
+++ hub/main.go
@@ -60,11 +60,6 @@ func (h *Hub) HandleSubscribe(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Missing or invalid charset; only UTF-8 is supported", http.StatusUnsupportedMediaType)
 		return
 	}
-	if charset, ok := params["charset"]; ok && strings.ToLower(charset) != "utf-8" {
-		http.Error(w, "Unsupported Charset", http.StatusUnsupportedMediaType)
-		return
-	}
-
 	parseErr := r.ParseForm()
 	if parseErr != nil {
 		http.Error(w, "Invalid subscription request", http.StatusBadRequest)
@@ -83,26 +78,33 @@ func (h *Hub) HandleSubscribe(w http.ResponseWriter, r *http.Request) {
 		topic == "" ||
 		(mode != "subscribe" && mode != "unsubscribe") ||
 		(mode == "subscribe" && (secret == "" || len(secret) > 199)) {
-
 		http.Error(w, "Invalid subscription request", http.StatusBadRequest)
 		if URLErr == nil {
-			q := denialURL.Query()
-			q.Set("hub.mode", "denied")
-			q.Set("hub.topic", topic)
-			denialURL.RawQuery = q.Encode()
-			http.Get(denialURL.String())
+			sendDenial(denialURL, topic)
 		} else {
-			log.Printf("invalid callback URL: %v", err)
+			log.Printf("invalid callback URL: %v", URLErr)
 		}
 		return
 	}
+	w.WriteHeader(http.StatusAccepted)
+	key := subscriptionKey{Callback: callback, Topic: topic}
+	if mode == "subscribe" {
+		go verifySub(key, h, secret, denialURL)
+	}
+	if mode == "unsubscribe" {
+		h.mu.Lock()
+		delete(h.subscribers, key)
+		h.mu.Unlock()
+	}
+}
 
-	// Intent verification
+func verifySub(key subscriptionKey, hub *Hub, secret string, denialURL *url.URL) {
 	challenge := generateRandomString(ChallengeLength)
-	verifyURL := fmt.Sprintf("%s?hub.mode=subscribe&hub.topic=%s&hub.challenge=%s", callback, topic, challenge)
+	format := "%s?hub.mode=subscribe&hub.topic=%s&hub.challenge=%s"
+	verifyURL := fmt.Sprintf(format, key.Callback, key.Topic, challenge)
 	resp, err := http.Get(verifyURL)
 	if err != nil {
-		http.Error(w, "Failed to reach subscriber", http.StatusBadGateway)
+		log.Printf("Failed to reach subscriber on verification: %v", err)
 		return
 	}
 	//close body, free resources
@@ -110,21 +112,28 @@ func (h *Hub) HandleSubscribe(w http.ResponseWriter, r *http.Request) {
 
 	body, bodyErr := io.ReadAll(resp.Body)
 	//trimming whitespaces in body for leniency
-	if resp.StatusCode != http.StatusOK || bodyErr != nil || strings.TrimSpace(string(body)) != challenge {
-		http.Error(w, "Intent verification failed", http.StatusBadRequest)
+	if resp.StatusCode != http.StatusOK ||
+		bodyErr != nil ||
+		strings.TrimSpace(string(body)) != challenge {
+		log.Printf("Intent verification failed: %v", err)
+		sendDenial(denialURL, key.Topic)
 		return
 	}
+	hub.mu.Lock()
+	hub.subscribers[key] = secret
+	hub.mu.Unlock()
+}
 
-	key := subscriptionKey{Callback: callback, Topic: topic}
-	h.mu.Lock()
-	if mode == "subscribe" {
-		h.subscribers[key] = secret
+func sendDenial(denialURL *url.URL, topic string) {
+	u := *denialURL
+	q := u.Query()
+	q.Set("hub.mode", "denied")
+	q.Set("hub.topic", topic)
+	u.RawQuery = q.Encode()
+	_, err := http.Get(u.String())
+	if err != nil {
+		log.Printf("Failed to send denial get request: %v", err)
 	}
-	if mode == "unsubscribe" {
-		delete(h.subscribers, key)
-	}
-	h.mu.Unlock()
-	w.WriteHeader(http.StatusAccepted)
 }
 
 func (h *Hub) HandlePublish(w http.ResponseWriter, r *http.Request) {
